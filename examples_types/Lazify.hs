eae8a1b1a14f5bac0a1958ee641db0d31cd53551
José Manuel Calderón Trilla <jmct+git@jmct.cc>
2014-06-25 19:06:47 +0100

    Added different variations of data-type declarations

--- examples_types/Lazify.hs
+++ examples_types/Lazify.hs
@@ -2,8 +2,12 @@
 
 data MyPair a b = MkPr a b;
 data Bool    = True | False ;
+data AbsPair = AP (MyPair Bool Bool);
 data List a  = Cons a (List a) | Nil ;
 data Triple a b c = A a | B b | C c | D a (Triple a b c);  
+data Shrub = Root Node;
+data Node = Void | Fork Shrub Shrub;
+data Rose a = R a (List (Rose a));
 
 map f Nil = Nil;
 map f (Cons x xs) = Cons (f x) (map f xs);
