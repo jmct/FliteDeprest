59aca6b699098d1a60e500c7bdceb194101b5ce6
José Manuel Calderón Trilla <jmct+git@jmct.cc>
2015-02-25 21:19:44 +0000

    Add tak.hs benchmark

---
+++ examples_types/tak.hs
@@ -0,0 +1,13 @@
+{
+data Bool = True | False;
+
+tak x y z = case (<=) x y of {
+            True  -> z ;
+            False -> tak (tak ((-) x 1) y z)
+		                     (tak ((-) y 1) z x)
+		                     (tak ((-) z 1) x y) ;
+            } ;
+
+main = tak 24 16 8;
+
+}
